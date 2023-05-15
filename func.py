854737d3b839692ab7ef05aca1f9f86f7d3800fa
Agun Akindele <aguntoroti10@gmail.com>
2023-05-15 13:14:48 +0100

    i just do it

--- func.py
+++ func.py
@@ -5,19 +5,30 @@
 #
 #
 # func_1()
+# def add(a, b):
+#     c = a + b
+#     return c
+
+
+# result = add(10, 20)
+# print(result)
+# add(100, 200)
+
+# def mult(a, b):
+#     print(a * b)
+#
+#
+# mult(10, 20)
+# mult(100, 200)
+
+
 def add(a, b):
-    c = a + b
-    return c
+    return a + b
 
 
-result = add(10, 20)
-print(result)
-add(100, 200)
+def square(c):
+    return c * c
 
 
-def mult(a, b):
-    print(a * b)
-
-
-mult(10, 20)
-mult(100, 200)
+results = square(add(2, 4))
+print(results)
